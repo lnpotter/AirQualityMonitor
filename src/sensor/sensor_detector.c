0d78ba9163c7cf6ee2fc35c880fc04b7a9f3b0b2
lnpotter <lucasnoalprincipal@gmail.com>
2026-05-20 12:14:58 -0300

    refactor(sensor): use shared plugin path resolution

--- src/sensor/sensor_detector.c
+++ src/sensor/sensor_detector.c
@@ -1,43 +1,10 @@
 #include "sensor/sensor_detector.h"
 #include "core/globals.h"
 #include "core/aqm_platform.h"
+#include "sensor/sensor_utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-static const char *resolve_default_plugin_path(const char *mode) {
-    if (!mode)
-        return NULL;
-#ifdef _WIN32
-    if (strcmp(mode, "dht22") == 0)
-        return ".\\plugins\\dht22_plugin.dll";
-    if (strcmp(mode, "bme680") == 0)
-        return ".\\plugins\\bme680_plugin.dll";
-    if (strcmp(mode, "pms5003") == 0)
-        return ".\\plugins\\pms5003_plugin.dll";
-    if (strcmp(mode, "mh-z19") == 0 || strcmp(mode, "mhz19") == 0)
-        return ".\\plugins\\mhz19_plugin.dll";
-#elif __APPLE__
-    if (strcmp(mode, "dht22") == 0)
-        return "./plugins/dht22_plugin.dylib";
-    if (strcmp(mode, "bme680") == 0)
-        return "./plugins/bme680_plugin.dylib";
-    if (strcmp(mode, "pms5003") == 0)
-        return "./plugins/pms5003_plugin.dylib";
-    if (strcmp(mode, "mh-z19") == 0 || strcmp(mode, "mhz19") == 0)
-        return "./plugins/mhz19_plugin.dylib";
-#else
-    if (strcmp(mode, "dht22") == 0)
-        return "./plugins/dht22_plugin.so";
-    if (strcmp(mode, "bme680") == 0)
-        return "./plugins/bme680_plugin.so";
-    if (strcmp(mode, "pms5003") == 0)
-        return "./plugins/pms5003_plugin.so";
-    if (strcmp(mode, "mh-z19") == 0 || strcmp(mode, "mhz19") == 0)
-        return "./plugins/mhz19_plugin.so";
-#endif
-    return NULL;
-}
 
 int detect_available_sensors(DetectedSensor *detected_sensors, int max_sensors) {
     const char *sensor_modes[] = {"dht22", "bme680", "pms5003", "mhz19"};
@@ -46,7 +13,6 @@ int detect_available_sensors(DetectedSensor *detected_sensors, int max_sensors) 
     for (size_t i = 0; i < sizeof(sensor_modes) / sizeof(sensor_modes[0]) && count < max_sensors; i++) {
         const char *mode = sensor_modes[i];
         const char *plugin_path = resolve_default_plugin_path(mode);
-        
         if (!plugin_path) {
             continue;
         }
@@ -62,17 +28,13 @@ int detect_available_sensors(DetectedSensor *detected_sensors, int max_sensors) 
         SensorModule module;
         if (sensor_module_load(plugin_path, &module) == 0) {
             ds->detected = 1;
-            // Copy the plugin name to our buffer before unloading
             if (module.plugin->name) {
                 snprintf(ds->plugin_name, sizeof(ds->plugin_name), "%s", module.plugin->name);
             }
-            
-            // Try to initialize the sensor to check if it's actually available
             if (module.plugin->init() == 0) {
                 ds->can_initialize = 1;
                 module.plugin->shutdown();
             }
-            
             sensor_module_unload(&module);
         }
 
@@ -121,14 +83,14 @@ void run_auto_detection_and_prompt(void) {
 
     printf("%d sensor(s) appear to be connected and ready.\n", available_count);
     printf("\nDo you want to enable these sensors for multi-sensor data collection? (y/n): ");
-    
+
     char input[8];
     if (fgets(input, sizeof(input), stdin) && (input[0] == 'y' || input[0] == 'Y')) {
         printf("\nEnabling detected sensors...\n");
-        
+
         init_sensor_configs();
         active_sensor_count = 0;
-        
+
         for (int i = 0; i < count; i++) {
             if (detected_sensors[i].detected && detected_sensors[i].can_initialize) {
                 if (active_sensor_count < MAX_SENSORS) {
@@ -136,13 +98,13 @@ void run_auto_detection_and_prompt(void) {
                     snprintf(config->mode, sizeof(config->mode), "%s", detected_sensors[i].mode);
                     snprintf(config->plugin_path, sizeof(config->plugin_path), "%s", detected_sensors[i].plugin_path);
                     config->enabled = 1;
-                    
+
                     printf("  - Enabled %s (sensor_id: %d)\n", detected_sensors[i].plugin_name[0] ? detected_sensors[i].plugin_name : detected_sensors[i].mode, active_sensor_count);
                     active_sensor_count++;
                 }
             }
         }
-        
+
         if (active_sensor_count > 0) {
             printf("\nSuccessfully enabled %d sensor(s) for multi-sensor data collection.\n", active_sensor_count);
             printf("Configuration will be saved when you exit.\n");
